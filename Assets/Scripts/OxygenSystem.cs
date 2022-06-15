237bad83eaa1fbdb4ef8fd30ad8f92c7e6b99f45
PrimeLockerbee <bradleyvanewijk@gmail.com>
2022-06-15 15:49:02 +0200

    Fixed player UI, it works now.

--- Assets/Scripts/OxygenSystem.cs
+++ Assets/Scripts/OxygenSystem.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OxygenSystem : MonoBehaviour
 {
     [SerializeField]
     public float i_PlayerOxygen = 100;
 
+    [SerializeField]
+    Text t_OxygenText;
+
     void Start()
     {
         InvokeRepeating("Subtract", 1f, 1f);
+    }
+
+    private void Update()
+    {
+        t_OxygenText.text = i_PlayerOxygen.ToString();
     }
 
     void Subtract()
--- Assets/Scripts/TempratureSystem.cs
+++ Assets/Scripts/TempratureSystem.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TempratureSystem : MonoBehaviour
 {
     [SerializeField]
     public float i_PlayerTemp = 37;
 
+    [SerializeField]
+    Text t_TempratureText;
+
     void Start()
     {
         InvokeRepeating("Subtract", 1f, 1f);
+    }
+
+    private void Update()
+    {
+        t_TempratureText.text = i_PlayerTemp.ToString();
     }
 
     void Subtract()
